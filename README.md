172a920547212de2f0dff56e991cb06e89013685
alchemyunited <mark.simchock@alchemyunited.com>
2014-11-17 18:19:50 -0500

    push 0

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-class-wp-ezclasses-docs-boilerplate-ezclass-1
-=============================================
+# Org: WPezClasses
+### Product: Class_WP_ezClasses_Docs-Boilerplate-ezClass-1
 
-Boilerplate class for starting an WPezClasses product.
+##### Boilerplate class for starting an WPezClasses product. For more details: https://github.com/WPezClasses
+
+This is a work in progress and isn't intended to be a standard as much as a sketch of a solid jumping in point.
---
+++ TODO_class-wp-ezclasses-docs-boilerplate-ezclass-1.php
@@ -0,0 +1,85 @@
+<?php
+/** 
+ * TODO
+ *
+ * TODO
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: TODO
+ *
+ * @package WPezClasses
+ * @author Mark Simchock <mark.simchock@alchemyunited.com>
+ * @since 0.5.1
+ * @license TODO
+ */
+ 
+/**
+ * == Change Log == 
+ *
+ * -- 0.5.0 - Mon 17 Nov 2014
+ *
+ * ---- Pop the champagne!
+ */
+ 
+/**
+ * == TODO == 
+ *
+ *
+ */
+
+// No WP? Die! Now!!
+if (!defined('ABSPATH')) {
+	header( 'HTTP/1.0 403 Forbidden' );
+    die();
+}
+
+if ( ! class_exists('Class_WP_ezClasses_TODO-FOLDER_TODO-Product_#') ) {
+  class Class_WP_ezClasses_TODO-FOLDER_TODO-Product_# extends Class_WP_ezClasses_Master_Singleton{
+  
+    protected $_arr_init;
+	
+    protected $_file;	
+	protected $_url;
+	protected $_path;
+  
+	public function __construct() {
+	  parent::__construct();
+	}
+		
+	/**
+	 *
+	 */
+	public function ezc_init($arr_args = ''){
+	
+	  $this->setup();
+	
+	  $arr_init_defaults = $this->init_defaults();
+	  
+	  $this->_arr_init = WP_ezMethods::ez_array_merge(array($arr_init_defaults, $arr_args));
+	
+	}
+	
+	/**
+	 * 
+	 */
+	protected setup(){
+	  
+	  $this->_file = __FILE__ ;
+	  $this->_url = plugin_dir_url(__FILE__);
+	  $this->_path = plugin_dir_path(__FILE__);
+	}
+	
+	/**
+	 *
+	 */
+	protected function init_defaults(){
+	
+	  $arr_defaults = array();
+	
+	  return $arr_defaults;
+	}
+	  
+  
+  }
+} 
